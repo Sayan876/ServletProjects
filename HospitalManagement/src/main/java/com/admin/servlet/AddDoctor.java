7707ec591c9b8d0587bfd330a14b21b6f8bb84c4
Sayan876 <120472214+Sayan876@users.noreply.github.com>
2024-06-27 17:30:01 +0530

    Add files via upload

---
+++ HospitalManagement/src/main/java/com/admin/servlet/AddDoctor.java
@@ -0,0 +1,44 @@
+package com.admin.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.Dao.DoctorDao;
+import com.Db.DBConnect;
+import com.entity.Doctor;
+
+@WebServlet("/addDoctor")
+public class AddDoctor extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String fullname = req.getParameter("fullname");
+		String dob = req.getParameter("dob");
+		String qualification = req.getParameter("qualification");
+		String specialist = req.getParameter("specialist");
+		String email = req.getParameter("email");
+		String mobno = req.getParameter("mobno");
+		String password = req.getParameter("password");
+		
+		Doctor d = new Doctor(fullname, dob, qualification, specialist, email, mobno, password);
+		
+		DoctorDao ddao = new DoctorDao(DBConnect.getConn());
+		HttpSession session =req.getSession();
+		
+		if(ddao.registerDoctor(d)) {
+			session.setAttribute("sucMsg", "Doctor added");
+			resp.sendRedirect("admin/doctor.jsp");
+		}else {
+			session.setAttribute("errorMsg", "Something wrong on the server");
+			resp.sendRedirect("admin/doctor.jsp");
+		}
+		
+	}
+
+}
